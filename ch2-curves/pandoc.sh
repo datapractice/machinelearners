a3a4195495c1d1814062d03f6cd86011e8e9df26
rian39 <a.mackenzie@lancaster.ac.uk>
2013-07-18 10:45:34 +0100

    developing closed-open form function finding discussion

--- ch2-curves/pandoc.sh
+++ ch2-curves/pandoc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pandoc --smart --normalize --latex-engine=xelatex  --template=../template.latex --bibliography=../references/refs.bib ch2_curves_function.md -o ch2_curves_function.pdf
+pandoc --smart --normalize --latex-engine=xelatex   --bibliography=../references/refs.bib ch2_curves_function.md -o ch2_curves_function.pdf
