84fc90b519bb1331827e19c4812cdb1a71a2f4c3
ecurbnairda <a.mackenzie@lancaster.ac.uk>
2014-10-14 22:22:51 +0100

    trying to develop topic model analysis of topic model analysis

--- ch4_probability/topics.R
+++ ch4_probability/topics.R
@@ -4,3 +4,7 @@ library(tm)
 library(tm.plugin.mail)
 library(SnowballC)
 library(topicmodels)
+
+path = '../ml_lit/data/topicmodel_WOS/'
+read.csv(
+
--- ml_lit/topicmodel_lit_analysis.py
+++ ml_lit/topicmodel_lit_analysis.py
@@ -1,5 +1,4 @@
 
-import wxversion
 import ml_lit_anal as ml
 import gensim
 from gensim import corpora
@@ -9,32 +8,51 @@ from gensim import corpora, models, similarities
 from itertools import chain
 from operator import itemgetter
 
-def topicmodel(files):
-    
-tm = ml.load_records('data/expect_max_WOS/')
-documents = tm.AB.dropna().tolist()
+def construct_topicmodel(n_topics = 60, files='data/expect_max_WOS/', document_to_display = 1):
+    """
+    Loads all the files in the folder as a dataframe, runs an LDA topic model
+    on them using n_topics
+    """
+    tm = ml.load_records(files)
+    ab_title = tm.TI + ' ' + tm.AB
+    documents = ab_title.dropna().tolist()
+    stoplist = stopwords.words('english')
+    stoplist = stoplist + ['topic', 'model','models', 'topics', 'data']
+    texts = [[word for word in document.lower().split() if word not in stoplist] for document in documents]
+    dictionary = corpora.Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    tfidf = models.TfidfModel(corpus) 
+    corpus_tfidf = tfidf[corpus]
+    lda = models.LdaMulticore(corpus_tfidf, id2word=dictionary, num_topics=n_topics, workers = 3)
+    for i in range(0, n_topics):
+         temp = lda.show_topic(i, 10)
+         terms = []
+         for term in temp:
+             terms.append(term[1])
+         print "Top 10 terms for topic #" + str(i) + ": "+ ", ".join(terms)
+    print 
+    print 'Which LDA topic maximally describes a document?\n'
+    print 'Original document: ' + documents[document_to_display]
+    print 'Preprocessed document: ' + str(texts[document_to_display])
+    #print 'Matrix Market format: ' + str(corpus[document_to_display])
+    print 'Topic probability mixture: ' + str(lda[corpus[document_to_display]])
+    print 'Maximally probable topic: topic #' + str(max(lda[corpus[document_to_display]],key=itemgetter(1))[0])
+    return {'model':lda, 'corpus':corpus, 'text':texts, 'documents':documents}
 
-stoplist = stopwords.words('english')
-texts = [[word for word in document.lower().split() if word not in stoplist]
- for document in documents]
-dictionary = corpora.Dictionary(texts)
-corpus = [dictionary.doc2bow(text) for text in texts]
+def predict_document_topic(model, corpus, texts, documents, document_to_display):
+    print 
+    print 'Which LDA topic maximally describes a document?\n'
+    print 'Original document: ' + documents[document_to_display]
+    print 'Preprocessed document: ' + str(texts[document_to_display])
+    print 'Matrix Market format: ' + str(corpus[document_to_display])
+    print 'Topic probability mixture: ' + str(model[corpus[document_to_display]])
+    print 'Maximally probable topic: topic #' + str(max(model[corpus[document_to_display]],key=itemgetter(1))[0])
+    max_topic = max(model[corpus[document_to_display]],key=itemgetter(1))[0]
+    print model.show_topic(max_topic)
 
-tfidf = models.TfidfModel(corpus) 
-corpus_tfidf = tfidf[corpus]
-n_topics = 60
-lda = models.LdaModel(corpus_tfidf, id2word=dictionary, num_topics=n_topics)
-for i in range(0, n_topics):
-     temp = lda.show_topic(i, 10)
-     terms = []
-     for term in temp:
-         terms.append(term[1])
-     print "Top 10 terms for topic #" + str(i) + ": "+ ", ".join(terms)
-        
-print 
-print 'Which LDA topic maximally describes a document?\n'
-print 'Original document: ' + documents[1]
-print 'Preprocessed document: ' + str(texts[1])
-print 'Matrix Market format: ' + str(corpus[1])
-print 'Topic probability mixture: ' + str(lda[corpus[1]])
-print 'Maximally probable topic: topic #' + str(max(lda[corpus[1]],key=itemgetter(1))[0])
+def predict_document(res, document_to_display):
+    model = res['model']
+    corpus = res['corpus']
+    documents = res['documents']
+    texts = res['text']
+    predict_document_topic(model, corpus, texts, documents, document_to_display)
