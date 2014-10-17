82c7c83f680d70879e305e5140d3425cfcb3ae28
ecurbnairda <a.mackenzie@lancaster.ac.uk>
2014-10-17 12:58:22 +0100

    trying to put the book together; doing more on ch4 on parameter estimation

--- ch2_curves/pandoc.sh
+++ ch2_curves/pandoc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pandoc --smart --normalize --latex-engine=xelatex   --bibliography=../references/refs.bib ch2_curves_function.md -o ch2_curves_function.pdf
+pandoc --smart --normalize --latex-engine=xelatex   --bibliography=~/Documents/ref_bibs/data_forms_thought.bib ch2_curves_function.md -o ch2_curves_function.pdf
