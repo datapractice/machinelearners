6289e2db31c92e5959e466f45857e77d076432fb
ecurbnairda <a.mackenzie@lancaster.ac.uk>
2014-10-28 22:32:20 +0000

    starting to pull diagram through

--- ml_lit/topicmodel_lit_analysis.py
+++ ml_lit/topicmodel_lit_analysis.py
@@ -1,4 +1,3 @@
-
 import ml_lit_anal as ml
 import gensim
 import nltk
@@ -10,19 +9,23 @@ from gensim import corpora, models, similarities
 from itertools import chain
 from operator import itemgetter
 from nltk.tokenize import word_tokenize
-
 files='data/expect_max_WOS/'
 tm = ml.load_records(files)
 ab_title = tm.TI + ' ' + tm.AB
 documents = ab_title.dropna().tolist()
-abstracts = [pos_tag(word_tokenize(s)) for s in documents[:1000]]
-workers = 7
-n_topics = 20
-iterate=50
-texts_ngram =  [[w[0] for w in ab if w[1] in 'NN' or w[1] in 'JJ'] for ab in abstracts]
+abstracts = [pos_tag(word_tokenize(s)) for s in documents]
+abstracts[0]
+texts =  [[w[0].lower() for w in ab if w[1] in 'NNS' or w[1] in 'JJR'] for ab in abstracts]
+texts =  [[w[0].lower() for w in ab if w[1] in 'VB'] for ab in abstracts]
+texts_ngram = [ngrams(t, 1) for t in texts]
+texts_ngram = [[' '.join(t).replace(',', '').replace('.', '') for t in tex] for tex in texts_ngram]
+dictionary = corpora.Dictionary(texts_ngram)
 corpus = [dictionary.doc2bow(text) for text in texts_ngram]
 tfidf = models.TfidfModel(corpus)
 corpus_tfidf = tfidf[corpus]
+workers = 3
+n_topics = 20
+iterate=150
 lda = models.LdaMulticore(corpus_tfidf, id2word=dictionary, iterations=iterate, num_topics=n_topics, workers = workers)
 for i in range(0, n_topics):
         temp = lda.show_topic(i, 10)
@@ -30,7 +33,7 @@ for i in range(0, n_topics):
         for term in temp:
             terms.append(term[1])
         print "Top 10 terms for topic #" + str(i) + ": "+ ", ".join(terms)
-print
+        print
 
 
 
---
+++ title.md
@@ -0,0 +1,9 @@
+# Into the data: diagramming machine learning
+## Forms of data thought: entanglements of subjectivity and computation
+## Forms of data thought: learning from machines
+## Forms of data thought: what to do with machine learning?
+## Learning from data: 
+## Reconstruction in data: forms of machine thought
+## Recursions and reconstructions: 
+## What can you do with machine learning?
+## Envisaging data
