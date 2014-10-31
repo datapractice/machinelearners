ca30d37b4ae997d5b7f384f2325d0149b5330369
MACKENZA <a.mackenzie@lancaster.ac.uk>
2014-10-31 14:10:02 +0000

    formatting ch1 for revision

--- ch1_learning/pandoc.sh
+++ ch1_learning/pandoc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pandoc --smart --normalize --latex-engine=xelatex  --template=../template.latex --bibliography=../references/refs.bib  --bibliography=../references/data_forms_thought.bib  --bibliography=../references/machine_learning.bib --bibliography=../references/R.bib --bibliography=../references/ch5_refs.bib ch1_praxis.md -o ch1_praxis.pdf
+pandoc --smart --normalize --latex-engine=xelatex  --template=../template.latex --bibliography=..//ref_bibs//refs.bib  --bibliography=..//ref_bibs//data_forms_thought.bib  --bibliography=..//ref_bibs//machine_learning.bib --bibliography=..//ref_bibs//R.bib --bibliography=..//ref_bibs//ch5_refs.bib ch1_praxis.md -o ch1_praxis.pdf
