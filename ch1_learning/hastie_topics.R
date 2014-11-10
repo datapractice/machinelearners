4d52e618a07fa5614840c9d67ef68937c8ff847f
MACKENZA <a.mackenzie@lancaster.ac.uk>
2014-11-10 16:50:32 +0000

    trying R topic models on hastie

--- ch1_learning/hastie_topics.R
+++ ch1_learning/hastie_topics.R
@@ -1,17 +1,18 @@
 library(mallet) 
 library(tm)
-system('pdftotext -f 10 -l 716 ../ml_lit/data/hastie_elem/hastie_elements_2009.pdf hastie_main.txt')
+writeLines(stopwords(), 'en.txt')
+system('pdftotext -f 1 -l 716 ../ml_lit/data/hastie_elem/hastie_elements_2009.pdf hastie_main.txt')
 main_text = readLines('hastie_main.txt')
 main_text_clean = paste(main_text, ' ', collapse=' ')
-words = strsplit(main_text_clean, ' ')
-
-words = tolower(main_text)
+words = unlist(strsplit(main_text_clean, ' '))
+words = tolower(words)
 words = words[!words == '']
 words = words[!words == '.']
-
+length(words)
 hastie_chunks = split(words, ceiling(seq_along(words)/1000))
 
 ids = 1:length(hastie_chunks)
+length(hastie_chunks)
 df = data.frame(ids = ids, text = hastie_chunks, stringsAsFactors = FALSE )
 mall = mallet.import(ids, hastie_chunks,'en.txt') 
 topic.model = MalletLDA(20)
