c8a9a7ff7a15cc8788db883a9aa072145d67d39f
ecurbnairda <a.mackenzie@lancaster.ac.uk>
2015-07-01 22:01:44 +0100

    cleaning up kaggle data

--- ch7_subjects/kaggle.py
+++ ch7_subjects/kaggle.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
-import seaborn
 import requests
 import pandas as pd
 url ='https://www.kaggle.com/competitions/search?SearchVisibility=AllCompetitions&ShowCompleted=true&ShowProspect=true&ShowOpenToAll=true&ShowPrivate=true&ShowLimited=true&DeadlineColumnSort=Ascending'
-r = requests.get(url)
+url2 = 'https://www.kaggle.com/competitions/search?SearchVisibility=AllCompetitions&ShowActive=true&ShowCompleted=true&ShowProspect=true&ShowOpenToAll=true&ShowPrivate=true&ShowLimited=true&DeadlineColumnSort=Ascending'
+r = requests.get(url2)
 b = BeautifulSoup(r.content)
 table = b.find('table')
 headings = [th.get_text() for th in table.find("tr").find_all("th")]
@@ -13,7 +13,10 @@ for row in table.find_all("tr")[1:]:
         datasets.append( [td.get_text() for td in row.find_all('td')])
 df = pd.DataFrame.from_records(datasets)
 df.columns = headings
-df.head()
-df['Competition'] = df['Competition Name'].str.strip()
+df['Competition'] = df['Competition Name'].str.strip().str.replace('\W+', ' ')
 df.Teams = df.Teams.astype('int')
-df[['Teams', 'Reward']].plot()
+reward = df.Reward
+reward = reward.str.replace('[,\$]', '')
+reward = reward.str.replace('Knowledge|Kudos|Jobs|Swag|Private', '0')
+df['Reward_amount'] = reward
+df.to_csv('kaggle_comp.csv', encoding='utf-8')
