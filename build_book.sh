b0a0403d448378e444bac748007fc880a20d9404
ecurbnairda <rian39@gmail.com>
2015-07-22 12:42:08 +0100

    playing with latex and google data

--- build_book.sh
+++ build_book.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/zsh
 
+# convert all rmd to md using knitr
+#
+# give list of md to pandoc to produce one latex document
 rm book.rmd
-xargs -a chapter_list.txt cat >>book.rmd
+xargs -a chapter_list.txt pandoc -o book.latex
+#xargs -a chapter_list.txt cat >>book.rmd
