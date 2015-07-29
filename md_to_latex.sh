23fbca14ffbef7ea992f347c5e143f48db2e8237
ecurbnairda <a.mackenzie@lancaster.ac.uk>
2015-07-29 12:42:34 +0100

    cleaning up after big error cleanup

--- md_to_latex.sh
+++ md_to_latex.sh
@@ -6,3 +6,30 @@
 pandoc --biblatex title.md -o title.tex
 pandoc --biblatex acknowledgments.rmd -o acknowledgments.tex
 pandoc --biblatex preface.rmd -o preface.tex
+cd ch0_introduction/
+pandoc --biblatex ch_introduction.md -o ch.tex
+cd ..
+cd ch1_learning/
+pandoc --biblatex ch_praxis.md -o ch.tex
+cd ..
+cd ch2_vector/
+pandoc --biblatex ch_vector_space.md -o ch.tex
+cd ..
+cd ch3_curves/
+pandoc --biblatex ch_curves_functions.md -o ch.tex
+cd ..
+cd ch4_probability/
+pandoc --biblatex ch_naive_informed.md -o ch.tex
+cd ..
+cd ch5_dimensionality/
+pandoc --biblatex ch_dimensional_exuberance.md -o ch.tex
+cd ..
+cd ch6_topologies/
+pandoc --biblatex ch_genomic_topologies.md -o ch.tex
+cd ..
+cd ch7_subjects/
+pandoc --biblatex ch_learning_subjects.md -o ch.tex
+cd ..
+cd ch8_conclusion/
+pandoc --biblatex ch_conclusion.md -o ch.tex
+cd ..
