8cb4d75181b9b928138330d2dba8d753a0d5bd5e
ecurbnairda <a.mackenzie@lancaster.ac.uk>
2015-10-07 23:10:21 +0100

    index work

---
+++ index_to_table.r
@@ -0,0 +1,8 @@
+library(stringr)
+v = read.csv('index_fin_by_chapter.md', sep=':', header=FALSE)
+colnames(v) = c('ch', 'term')
+v$term_clean = gsub(str_extract(v$term, '\\{.+\\}'), pattern ='[{}|()]', replace='')
+v$ch = str_extract(v$ch, '\\w+(\\d_)?(\\w+)?/')
+tab = table(v$term_clean, v$ch)
+write.csv(tab, 'index_table.csv')
+view(as.data.frame(tab))
