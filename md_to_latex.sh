58051a5473506654e0b9dd12b1c3a80b49279ec7
ecurbnairda <a.mackenzie@lancaster.ac.uk>
2015-10-28 17:59:45 +0000

    building

--- md_to_latex.sh
+++ md_to_latex.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-pandoc --smart --normalize --latex-engine=xelatex  technique_demos.md -o technique_demos.pdf 
-pandoc --smart --normalize --latex-engine=xelatex  --template=template.latex --bibliography=~/Documents/ref_bibs/data_forms_thought.bib --bibliography=references/machine_learning.bib --bibliography=references/ch5_refs.bib proposal.rmd -o proposal.pdf 
+#pandoc --smart --normalize --latex-engine=xelatex  technique_demos.md -o technique_demos.pdf 
+#pandoc --smart --normalize --latex-engine=xelatex  --template=template.latex --bibliography=~/Documents/ref_bibs/data_forms_thought.bib --bibliography=references/machine_learning.bib --bibliography=references/ch5_refs.bib proposal.rmd -o proposal.pdf 
 
 pandoc --biblatex title.md -o title.tex
 echo '\documentclass[book.tex]{subfiles}\n' | cat - title.tex > temp && mv temp title.tex
